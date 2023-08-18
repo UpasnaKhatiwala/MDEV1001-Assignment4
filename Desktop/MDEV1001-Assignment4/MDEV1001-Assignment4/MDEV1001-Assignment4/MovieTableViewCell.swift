5871544877a05d5c0a2e0c544c8997f41011836c
khushishukla <shuklakhushi3110>
2023-08-18 15:30:25 -0400

    added firebasecrudviewcontroller and addeditfirestorecrudviewcontroller

---
+++ Desktop/MDEV1001-Assignment4/MDEV1001-Assignment4/MDEV1001-Assignment4/MovieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.swift
+//  MDEV1001-Assignment4
+//
+//  Created by Upasna Khatiwala on 2023-08-18.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UITextField!
+    
+    @IBOutlet weak var studioLabel: UITextField!
+    
+    @IBOutlet weak var ratingLabel: UITextField!
+}
